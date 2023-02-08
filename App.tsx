20271b26b198fd2c763401345bd341eb23adbea5
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-02-09 03:22:28 +0800

    add tailwind

--- App.tsx
+++ App.tsx
@@ -1,22 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import React from 'react'
+import CustomScreen from './screens/CustomScreen'
+import utilities from './tailwind.json'
+import { TailwindProvider } from 'tailwind-rn'
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    // @ts-ignore tailwind-rn doesn't have types yet
+    <TailwindProvider utilities={utilities}>
+      <CustomScreen />
+    </TailwindProvider>
+  )
 }
